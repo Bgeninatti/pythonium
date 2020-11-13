8631c6e58cf9fc7b49b36ad4fa6b325e52491b23
Bruno Geninatti <brunogeninatti@gmail.com>
2020-11-13 12:09:54 +0000

    some dependency injection and extra console comands

--- pythonium/main.py
+++ pythonium/main.py
@@ -8,6 +8,8 @@ from . import __version__
 from .game import Game
 from .game_modes import ClassicMode
 from .metrics_collector import MetricsCollector
+from .helpers import random_name
+from .logger import get_logger
 
 HELP_EPILOG = "A space strategy algorithmic-game build in python"
 
@@ -20,7 +22,20 @@ def go():
     parser.add_argument(
         '--players', action="extend", nargs="+", help="")
     parser.add_argument(
-        '--metrics', action="store_true", default=False, help="")
+        '--metrics',
+        action="store_true",
+        default=False,
+        help="Generate a report with metrics of the game")
+    parser.add_argument(
+        '--verbose', action="store_true", default=False, help="Show logs in stdout")
+    parser.add_argument(
+        '--raise-exceptions',
+        action="store_true",
+        default=False,
+        help="If the commands computations (for any player) fails, raise "
+             "the exception and stop.")
+    parser.add_argument(
+        '--sector-name', default="", help="An identification for the game")
 
     args = parser.parse_args()
 
@@ -28,7 +43,11 @@ def go():
         print("Running 'pythonium' version", __version__)
         return 0
 
+    base_dir = os.getcwd()
     game_mode = ClassicMode()
+    sector_name = random_name(6)
+    logfile_path = os.path.join(base_dir, f"{sector_name}.log")
+    logger = get_logger(sector_name, filename=logfile_path, verbose=args.verbose)
 
     players = []
     for player_module in args.players:
@@ -37,15 +56,18 @@ def go():
         players.append(player)
 
     start = time.time()
-    game = Game(players, game_mode, logdir=os.getcwd())
+    game = Game(sector_name,
+                players,
+                game_mode,
+                logger=logger,
+                raise_exceptions=args.raise_exceptions)
     game.play()
     sys.stdout.write(f"Game ran in {round(time.time() - start, 2)} seconds\n")
 
     if args.metrics:
         sys.stdout.write("Building report...\n")
         start = time.time()
-        with open(os.path.join(os.getcwd(), game.logfile)) as logfile:
+        with open(logfile_path) as logfile:
             metrics = MetricsCollector(logfile)
         metrics.build_report()
         sys.stdout.write(f"Report built in {round(time.time() - start, 2)} seconds\n")
-
