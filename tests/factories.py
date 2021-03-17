10f7b5f613052f47afafaa2765be40e9b065e14c
Bruno Geninatti <brunogeninatti@gmail.com>
2021-03-17 01:13:33 +0000

    test(factories): adds Planet and CostVector factories

--- tests/factories.py
+++ tests/factories.py
@@ -1,21 +1,36 @@
 from faker import Faker
 
 from pythonium import Planet
+from pythonium.vectors import Transfer
+
+
+class TransferVectorFactory:
+
+    faker = Faker()
+
+    @classmethod
+    def build_transfer_vector(cls):
+        return Transfer(
+            megacredits=cls.faker.pyint(),
+            pythonium=cls.faker.pyint(),
+        )
 
 
 class PlanetFactory:
 
-    faker: Faker
+    faker = Faker()
 
-    def __call__(
-        self,
-        faker,
-    ):
+    @classmethod
+    def build_planet(cls, **kwargs):
+        mine_cost = TransferVectorFactory.build_transfer_vector()
         return Planet(
-            pid=self.faker.pyint(),
-            position=(self.faker.pyint(), self.faker.pyint()),
-            temperature=self.faker.pyint(),
-            underground_pythonium=self.faker.pyint(),
-            concentration=self.faker.pyfloat(),
-            pythonium=self.faker.pyint(),
+            pid=cls.faker.pyint(),
+            position=(cls.faker.pyint(), cls.faker.pyint()),
+            temperature=kwargs.get("temperature", cls.faker.pyint()),
+            underground_pythonium=cls.faker.pyint(),
+            concentration=kwargs.get(
+                "concentration", cls.faker.pyfloat(positive=True)
+            ),
+            pythonium=cls.faker.pyint(),
+            mine_cost=mine_cost,
         )
