bc0de8840e4db0cd2eb34a7769e0a7bc34fb7c62
Bruno Geninatti <brunogeninatti@gmail.com>
2022-03-24 14:15:38 -0300

    refactor: introduce OrderRequest

--- pythonium/planet.py
+++ pythonium/planet.py
@@ -113,7 +113,7 @@ class Planet(StellarThing):
     **Attribute that can be modified by the player**
 
     New mines that the player order to build in the current turn. This value is set to
-    zero when the player rules are executed.
+    zero when the player orders are executed.
     """
 
     new_ship: ShipType = attr.ib(
@@ -126,7 +126,7 @@ class Planet(StellarThing):
 
     The new ship that the player order to build in the current turn.
 
-    This value is set to ``None`` when the player rules are executed
+    This value is set to ``None`` when the player orders are executed
     """
 
     taxes: int = attr.ib(
@@ -255,7 +255,7 @@ class Planet(StellarThing):
 
     def get_orders(self):
         """
-        Compute rules based on player control attributes: ``new_mines``, \
+        Compute orders based on player control attributes: ``new_mines``, \
         ``new_ship`` and ``taxes``
         """
         orders = [
