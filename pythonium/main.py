4e1a3e305208cbc5250a87949bdbaf887d38a175
Bruno Geninatti <brunogeninatti@gmail.com>
2022-03-25 18:14:12 -0300

    feat: set random seed as click parameter

--- pythonium/main.py
+++ pythonium/main.py
@@ -1,11 +1,9 @@
-import argparse
-import importlib
-import logging
-import sys
+import random
+import uuid
 import time
-from pathlib import Path
-
 import click
+import importlib
+from pathlib import Path
 
 from . import __version__
 from .game import Game
@@ -15,6 +13,11 @@ from .logger import setup_logger
 from .output_handler import StandardOutputHanlder, StreamOutputHanlder
 
 HELP_EPILOG = "A space strategy algorithmic-game build in python"
+
+
+def set_seed(seed):
+    random.seed(seed)
+    uuid.uuid4 = lambda: uuid.UUID(version=4, int=random.getrandbits(128))
 
 
 @click.group(help=HELP_EPILOG)
@@ -28,6 +31,7 @@ def cli(ctx):
 @click.pass_context
 @click.argument("galaxy-name")
 @click.argument("players", nargs=-1)
+@click.option("--seed", default=time.time(), type=int)
 @click.option("--raise-exceptions/--no-raise-exceptions", default=False)
 @click.option("--verbose/--no-verbose", default=False)
 @click.option("--stream-state/--no-stream-state", default=False)
@@ -35,12 +39,14 @@ def run(
     ctx,
     galaxy_name,
     players,
+    seed,
     raise_exceptions,
     verbose,
     stream_state,
     *args,
     **kwargs,
 ):
+    set_seed(seed)
 
     game_mode = ClassicMode()
     galaxy_name = galaxy_name if galaxy_name else random_name(6)
